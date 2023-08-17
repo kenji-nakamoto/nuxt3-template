3fd25c2df38f4e0297e1465e912be2770d2ee22c
kenji-nakamoto <k.nakamoto.work@gmail.com>
2023-08-17 14:07:29 +0900

    add：tailwindの導入

---
+++ pages/index.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="w-[382px] h-[462px] relative shadow">
+    <div class="w-[382px] h-[462px] left-0 top-0 absolute bg-white rounded"></div>
+    <img
+      class="w-[382px] h-[233.76px] left-0 top-0 absolute rounded-tl rounded-tr"
+      src="https://via.placeholder.com/382x234"
+    />
+    <div
+      class="h-[139px] left-[24px] top-[250px] absolute flex-col justify-start items-start gap-2 inline-flex"
+    >
+      <div class="text-zinc-800 text-xl font-medium leading-[30px]">The Coldest Sunset</div>
+      <div class="w-[336px] h-[107px] text-zinc-700 text-base font-normal leading-relaxed">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla!
+        <br />
+        Maiores et perferendis eaque, exercitationem praesentium nihil.
+      </div>
+    </div>
+    <div class="left-[24px] top-[418px] absolute justify-start items-center gap-[11px] inline-flex">
+      <div class="justify-start items-center gap-2.5 flex">
+        <div class="px-4 py-[5px] bg-zinc-100 rounded-[999px] justify-start items-center flex">
+          <div class="text-center text-zinc-700 text-xs font-bold leading-[18px]">#photography</div>
+        </div>
+      </div>
+      <div class="justify-start items-center gap-2.5 flex">
+        <div class="px-4 py-[5px] bg-zinc-100 rounded-[999px] justify-start items-center flex">
+          <div class="text-center text-zinc-700 text-xs font-bold leading-[18px]">#travel</div>
+        </div>
+      </div>
+      <div class="justify-start items-center gap-2.5 flex">
+        <div class="px-4 py-[5px] bg-zinc-100 rounded-[999px] justify-start items-center flex">
+          <div class="text-center text-zinc-700 text-xs font-bold leading-[18px]">#winter</div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="mt-6">
+    <div class="w-80 h-[284px] relative bg-white rounded">
+      <div
+        class="left-[32px] top-[22px] absolute text-center text-zinc-800 text-sm font-bold leading-[21px]"
+      >
+        Username
+      </div>
+      <div
+        class="left-[32px] top-[103px] absolute text-center text-zinc-800 text-sm font-bold leading-[21px]"
+      >
+        Password
+      </div>
+      <div
+        class="left-[162px] top-[221px] absolute text-center text-blue-500 text-sm font-bold leading-[21px]"
+      >
+        Forgot Password?
+      </div>
+      <div class="left-[32px] top-[178px] absolute text-red-500 text-xs font-light">
+        Please choose a password
+      </div>
+      <div class="w-64 h-10 left-[32px] top-[49px] absolute">
+        <div class="w-64 h-10 left-0 top-0 absolute bg-white rounded border border-zinc-300">
+          <div class="w-[276px] h-[19px] left-[12px] top-[10px] absolute">
+            <div
+              class="w-[276px] left-0 top-[-2px] absolute text-zinc-500 text-base font-normal leading-normal"
+            >
+              Username
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="w-64 h-10 left-[32px] top-[130px] absolute">
+        <div class="w-64 h-10 left-0 top-0 absolute bg-white rounded border border-red-500">
+          <div class="w-[276px] h-[19px] left-[-32px] top-[10px] absolute"></div>
+          <div class="w-[276px] h-[19px] left-[12px] top-[10px] absolute">
+            <div
+              class="w-[276px] left-0 top-[-2px] absolute text-zinc-500 text-base font-normal leading-normal"
+            >
+              Password
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="left-[32px] top-[212px] absolute justify-start items-center gap-2.5 inline-flex">
+        <div class="px-4 py-2 bg-blue-500 rounded justify-start items-center flex">
+          <div class="text-center text-white text-base font-medium leading-normal">Sign In</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
