ddfa3a39ee773f744f0ba4082be2f7f00399d1a2
kenji-nakamoto <k.nakamoto.work@gmail.com>
2023-08-18 16:15:14 +0900

    add：viewport設定追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+    },
+  },
   css: ['~/assets/css/main.css'],
+  devtools: { enabled: true },
   postcss: {
     plugins: {
       tailwindcss: {},
