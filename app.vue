be2e1f349705ee43f516d7cae54b33132f896b97
kenji-nakamoto <k.nakamoto.work@gmail.com>
2023-08-18 10:49:12 +0900

    add：layout実装

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
-  <NuxtPage />
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
 <script lang="ts" setup>
 import { initFlowbite } from 'flowbite'
--- pages/index.vue
+++ pages/index.vue
@@ -1,169 +1,256 @@
 <template>
-  <div class="w-[382px] h-[462px] relative shadow">
-    <div class="w-[382px] h-[462px] left-0 top-0 absolute bg-white rounded"></div>
-    <img
-      class="w-[382px] h-[233.76px] left-0 top-0 absolute rounded-tl rounded-tr"
-      src="https://via.placeholder.com/382x234"
-    />
-    <div
-      class="h-[139px] left-[24px] top-[250px] absolute flex-col justify-start items-start gap-2 inline-flex"
-    >
-      <div class="text-zinc-800 text-xl font-medium leading-[30px]">The Coldest Sunset</div>
-      <div class="w-[336px] h-[107px] text-zinc-700 text-base font-normal leading-relaxed">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla!
-        <br />
-        Maiores et perferendis eaque, exercitationem praesentium nihil.
-      </div>
-    </div>
-    <div class="left-[24px] top-[418px] absolute justify-start items-center gap-[11px] inline-flex">
-      <div class="justify-start items-center gap-2.5 flex">
-        <div class="px-4 py-[5px] bg-zinc-100 rounded-[999px] justify-start items-center flex">
-          <div class="text-center text-zinc-700 text-xs font-bold leading-[18px]">#photography</div>
-        </div>
-      </div>
-      <div class="justify-start items-center gap-2.5 flex">
-        <div class="px-4 py-[5px] bg-zinc-100 rounded-[999px] justify-start items-center flex">
-          <div class="text-center text-zinc-700 text-xs font-bold leading-[18px]">#travel</div>
-        </div>
-      </div>
-      <div class="justify-start items-center gap-2.5 flex">
-        <div class="px-4 py-[5px] bg-zinc-100 rounded-[999px] justify-start items-center flex">
-          <div class="text-center text-zinc-700 text-xs font-bold leading-[18px]">#winter</div>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <div class="mt-6">
-    <div class="w-80 h-[284px] relative bg-white rounded">
-      <div
-        class="left-[32px] top-[22px] absolute text-center text-zinc-800 text-sm font-bold leading-[21px]"
-      >
-        Username
-      </div>
-      <div
-        class="left-[32px] top-[103px] absolute text-center text-zinc-800 text-sm font-bold leading-[21px]"
-      >
-        Password
-      </div>
-      <div
-        class="left-[162px] top-[221px] absolute text-center text-blue-500 text-sm font-bold leading-[21px]"
-      >
-        Forgot Password?
-      </div>
-      <div class="left-[32px] top-[178px] absolute text-red-500 text-xs font-light">
-        Please choose a password
-      </div>
-      <div class="w-64 h-10 left-[32px] top-[49px] absolute">
-        <div class="w-64 h-10 left-0 top-0 absolute bg-white rounded border border-zinc-300">
-          <div class="w-[276px] h-[19px] left-[12px] top-[10px] absolute">
-            <div
-              class="w-[276px] left-0 top-[-2px] absolute text-zinc-500 text-base font-normal leading-normal"
-            >
-              Username
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="w-64 h-10 left-[32px] top-[130px] absolute">
-        <div class="w-64 h-10 left-0 top-0 absolute bg-white rounded border border-red-500">
-          <div class="w-[276px] h-[19px] left-[-32px] top-[10px] absolute"></div>
-          <div class="w-[276px] h-[19px] left-[12px] top-[10px] absolute">
-            <div
-              class="w-[276px] left-0 top-[-2px] absolute text-zinc-500 text-base font-normal leading-normal"
-            >
-              Password
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="left-[32px] top-[212px] absolute justify-start items-center gap-2.5 inline-flex">
-        <div class="px-4 py-2 bg-blue-500 rounded justify-start items-center flex">
-          <div class="text-center text-white text-base font-medium leading-normal">Sign In</div>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <div>
-    <div class="flex justify-center p-4">
-      <button
-        id="button"
-        data-modal-toggle="modal"
-        data-modal-target="modal"
-        type="button"
-        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-      >
-        Show modal
-      </button>
-    </div>
-
-    <div
-      id="modal"
-      tabindex="-1"
-      aria-hidden="true"
-      class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full"
-    >
-      <div class="relative w-full max-w-2xl max-h-full">
-        <!-- Modal content -->
-        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-          <!-- Modal header -->
-          <div class="flex items-start justify-between p-5 border-b rounded-t dark:border-gray-600">
-            <h3 class="text-xl font-semibold text-gray-900 lg:text-2xl dark:text-white">
-              Terms of Service
-            </h3>
-            <button
-              id="closeButton"
-              data-modal-hide="modal"
-              type="button"
-              class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-              <svg
-                class="w-5 h-5"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-            </button>
-          </div>
-          <!-- Modal body -->
-          <div class="p-6 space-y-6">
-            <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-              With less than a month to go before the European Union enacts new consumer privacy
-              laws for its citizens, companies around the world are updating their terms of service
-              agreements to comply.
-            </p>
-            <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-              The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on
-              May 25 and is meant to ensure a common set of data rights in the European Union. It
-              requires organizations to notify users as soon as possible of high-risk data breaches
-              that could personally affect them.
-            </p>
-          </div>
-          <!-- Modal footer -->
-          <div
-            class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600"
+  <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+    <div class="grid grid-cols-3 gap-4 mb-4">
+      <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
+        <p class="text-2xl text-gray-400 dark:text-gray-500">
+          <svg
+            class="w-3.5 h-3.5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 18 18"
           >
-            <button
-              type="button"
-              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              I accept
-            </button>
-            <button
-              type="button"
-              class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600"
-            >
-              Decline
-            </button>
-          </div>
-        </div>
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M9 1v16M1 9h16"
+            />
+          </svg>
+        </p>
+      </div>
+      <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
+        <p class="text-2xl text-gray-400 dark:text-gray-500">
+          <svg
+            class="w-3.5 h-3.5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 18 18"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M9 1v16M1 9h16"
+            />
+          </svg>
+        </p>
+      </div>
+      <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
+        <p class="text-2xl text-gray-400 dark:text-gray-500">
+          <svg
+            class="w-3.5 h-3.5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 18 18"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M9 1v16M1 9h16"
+            />
+          </svg>
+        </p>
+      </div>
+    </div>
+    <div class="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
+      <p class="text-2xl text-gray-400 dark:text-gray-500">
+        <svg
+          class="w-3.5 h-3.5"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 18 18"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M9 1v16M1 9h16"
+          />
+        </svg>
+      </p>
+    </div>
+    <div class="grid grid-cols-2 gap-4 mb-4">
+      <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+        <p class="text-2xl text-gray-400 dark:text-gray-500">
+          <svg
+            class="w-3.5 h-3.5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 18 18"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M9 1v16M1 9h16"
+            />
+          </svg>
+        </p>
+      </div>
+      <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+        <p class="text-2xl text-gray-400 dark:text-gray-500">
+          <svg
+            class="w-3.5 h-3.5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 18 18"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M9 1v16M1 9h16"
+            />
+          </svg>
+        </p>
+      </div>
+      <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+        <p class="text-2xl text-gray-400 dark:text-gray-500">
+          <svg
+            class="w-3.5 h-3.5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 18 18"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M9 1v16M1 9h16"
+            />
+          </svg>
+        </p>
+      </div>
+      <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+        <p class="text-2xl text-gray-400 dark:text-gray-500">
+          <svg
+            class="w-3.5 h-3.5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 18 18"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M9 1v16M1 9h16"
+            />
+          </svg>
+        </p>
+      </div>
+    </div>
+    <div class="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
+      <p class="text-2xl text-gray-400 dark:text-gray-500">
+        <svg
+          class="w-3.5 h-3.5"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 18 18"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M9 1v16M1 9h16"
+          />
+        </svg>
+      </p>
+    </div>
+    <div class="grid grid-cols-2 gap-4">
+      <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+        <p class="text-2xl text-gray-400 dark:text-gray-500">
+          <svg
+            class="w-3.5 h-3.5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 18 18"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M9 1v16M1 9h16"
+            />
+          </svg>
+        </p>
+      </div>
+      <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+        <p class="text-2xl text-gray-400 dark:text-gray-500">
+          <svg
+            class="w-3.5 h-3.5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 18 18"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M9 1v16M1 9h16"
+            />
+          </svg>
+        </p>
+      </div>
+      <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+        <p class="text-2xl text-gray-400 dark:text-gray-500">
+          <svg
+            class="w-3.5 h-3.5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 18 18"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M9 1v16M1 9h16"
+            />
+          </svg>
+        </p>
+      </div>
+      <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+        <p class="text-2xl text-gray-400 dark:text-gray-500">
+          <svg
+            class="w-3.5 h-3.5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 18 18"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M9 1v16M1 9h16"
+            />
+          </svg>
+        </p>
       </div>
     </div>
   </div>
